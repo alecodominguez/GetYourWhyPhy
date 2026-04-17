1d6807a572f027e37effe0836c51f79b862ebdc7
alecodominguez <aleco@arizona.edu>
2026-04-16 22:39:55 -0700

    added html view

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, desc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pydantic import BaseModel
@@ -27,8 +29,9 @@ class WiFiLog(Base):
 
 Base.metadata.create_all(bind=engine)
 
-# 3. FastAPI App
+# 3. FastAPI App Initialization
 app = FastAPI()
+templates = Jinja2Templates(directory="templates") # call to index.html
 
 class WiFiData(BaseModel):
 	location: str
@@ -49,6 +52,16 @@ def log_wifi(data: WiFiData):
 	db.close()
 	return {"status": "success"}
 
-@app.get("/")
-def home():
-	return {"message": "WiFi Data Server is Online"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+	db = SessionLocal()
+    	try:
+        	# This query gets all logs, sorted by the newest first
+        	logs = db.query(WiFiLog).order_by(desc(WiFiLog.timestamp)).all()
+        
+        	return templates.TemplateResponse("index.html", {
+            		"request": request, 
+            		"logs": logs
+        	})
+    	finally:
+        	db.close()
