0ec07b499222cbe7bca529138c71162f4056b982
alecodominguez <aleco@arizona.edu>
2026-04-22 15:41:52 -0700

    added git pull cmd to start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,6 @@
 @echo off
 cd C:\Users\ual-laptop\GetYourWhyPhy
+git pull origin main
 call venv\Scripts\activate
 start "WhyPhy Server" uvicorn server:app --reload --port 8000
 start "Ngrok Tunnel" ngrok http --url=https://opacity-cadillac-emporium.ngrok-free.dev 8000
